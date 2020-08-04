0d23542c1b68e6f2784189eeb4a492e0336a142b
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-04 07:53:41 +0530

    Updates

---
+++ precache-manifest.ca1c47dd93451026d1ce43906be6d40f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fc2628dbdfaa073995cdee5dab0925",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "d3de288d195c451d52d2",
+    "url": "/dashboard/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "84fa034b4b3381bb21f4",
+    "url": "/dashboard/static/css/main.4924dea0.chunk.css"
+  },
+  {
+    "revision": "d3de288d195c451d52d2",
+    "url": "/dashboard/static/js/2.316a1ab6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dashboard/static/js/2.316a1ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fa034b4b3381bb21f4",
+    "url": "/dashboard/static/js/main.58148458.chunk.js"
+  },
+  {
+    "revision": "8f3ec870dce268db1d96",
+    "url": "/dashboard/static/js/runtime-main.2ec0fa51.js"
+  },
+  {
+    "revision": "8293ea879f8484dc4c001c326712f8b5",
+    "url": "/dashboard/static/media/dp.8293ea87.jpg"
+  }
+]);
