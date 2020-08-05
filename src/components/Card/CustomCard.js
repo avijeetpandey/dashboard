273e3638f95f3412de897b6ba26175a8b0967ad4
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-06 00:12:26 +0530

    card fix #05

--- src/components/Card/CustomCard.js
+++ src/components/Card/CustomCard.js
@@ -5,25 +5,25 @@ const {Text,Title} = Typography
 const CustomCard = ()=>{
     return(
         <div className="site-card-wrapper ctr c-text">
-    <Row gutter={16}>
-      <Col span={6}>
-        <Card title="REALTIME USERS" hoverable bordered={true}>
-          <Title>56</Title>
+    <Row gutter={16} >
+      <Col sm={6} span={24}>
+        <Card   style={{ marginTop: 16 }} className="g-text" title="REALTIME USERS" hoverable bordered={true}>
+          <Text className="title">56</Text>
         </Card>
       </Col>
-      <Col span={6}>
-        <Card title="TOTAL VISITS" hoverable bordered={true}>
-        <Title>54,598</Title>
+      <Col sm={6} span={24}>
+        <Card  style={{ marginTop: 16 }} className="g-text" title="TOTAL VISITS" hoverable bordered={true}>
+        <Text className="title">54,598</Text>
         </Card>
       </Col>
-      <Col span={6}>
-        <Card title="BOUNCERATE" hoverable bordered={true}>
-        <Title>73.67%</Title>
+      <Col sm={6} span={24}>
+        <Card  style={{ marginTop: 16 }} className="g-text" title="BOUNCE RATE" hoverable bordered={true}>
+        <Text className="title">73.67%</Text>
         </Card>
       </Col>
-      <Col span={6}>
-        <Card title="VISIT DURATION" hoverable bordered={true}>
-        <Title>1m 4s</Title>
+      <Col sm={6} span={24}>
+        <Card  style={{ marginTop: 16 }} className="g-text" title="VISIT DURATION" hoverable bordered={true}>
+        <Text className="title">1m 4s</Text>
         </Card>
       </Col>
     </Row>
