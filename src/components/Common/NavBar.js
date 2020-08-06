96bb1d4f0d2065c3c80abc2d6e66e67d3011e5f9
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-06 08:28:52 +0530

    final layout commit

--- src/components/Common/NavBar.js
+++ src/components/Common/NavBar.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { Typography } from "antd";
+import { Typography ,Dropdown,Button,Menu, Space} from "antd";
+import {
+  DownOutlined,
+  UserOutlined,
+  CalendarOutlined,
+} from "@ant-design/icons";
 
 const { Text, Title } = Typography;
 
@@ -9,12 +14,30 @@ const style = {
 };
 
 const NavBar = () => {
+  const menu = (
+    <Menu>
+      <Menu.Item key="1" icon={<UserOutlined />}>
+        ENG
+      </Menu.Item>
+      <Menu.Item key="2" icon={<UserOutlined />}>
+        FRENCH
+      </Menu.Item>
+      <Menu.Item key="3" icon={<UserOutlined />}>
+        RUSSIAN
+      </Menu.Item>
+    </Menu>
+  );
   return (
-    <div>
+    <Space style={{display:'flex',justifyContent:'space-between'}}>
       <Title level={4} style={style}>
         DashBoard
       </Title>
-    </div>
+      <Dropdown overlay={menu} className="dropdown">
+        <Button>
+          ENG <DownOutlined />
+        </Button>
+      </Dropdown>
+    </Space>
   );
 };
 
